6ae38eb0b3d4aa2e5c4485e654c5304bb747ea2c
andreamestizo <A01735773@tec.mx>
2024-08-15 18:48:34 -0600

    Update EQ1_Dia siguiente.py

--- EQ1_Dia siguiente.py
+++ EQ1_Dia siguiente.py
@@ -12,6 +12,13 @@ if year%4==0:
 if month==12 and day==31:
     print("El día siguiente es: ", year+1, "/", "1","/", "1" )
     exit()
+    
+if month==2:
+    if day==28:
+        print("El día siguiente es: ", year, "/", month+1,"/", "1" )
+        exit()
+    else:
+        print("El día siguiente es: ",year,"/", month,"/", day+1)
 
 if month%2==1 or month==8:
     if day==31:
