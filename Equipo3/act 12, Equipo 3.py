5696112c27511d36443c5138b770d4dd08f7479a
ValeriaGonzN <a00842141@tec.mx>
2024-08-26 17:54:38 -0600

    Add files via upload

---
+++ Equipo3/act 12, Equipo 3.py
@@ -0,0 +1,10 @@
+suma=0
+contador=0
+
+while suma<1000:
+    numero=float(input("escribe los numeros"))
+    suma=suma+numero
+    contador=contador+1
+
+print("la suma es igual a", suma)
+print(" la cantidad de numeros es igual a", contador)
