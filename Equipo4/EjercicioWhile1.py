f4df741972f287fe2a2fdc898ec75c7aae5ab342
A00842372 <A00842372@tec.mx>
2024-08-26 17:50:55 -0600

    Add files via upload

---
+++ Equipo4/EjercicioWhile1.py
@@ -0,0 +1,10 @@
+contador = 0
+contadorenteros = 0
+while contador <=1000.0:
+    num = float(input('Inserte un numero: '))
+    contador += num
+    contadorenteros+=1
+print(f'Tu numero fue {contador} y llegaste en {contadorenteros} numeros')
+
+
+
