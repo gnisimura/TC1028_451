652cf9d39a554c3f09179f5eab706b2bc0e3f68e
A00841165 <a00841165@tec.mx>
2024-08-26 18:02:58 -0600

    Add files via upload

---
+++ Equipo2/EQ2_While.py
@@ -0,0 +1,10 @@
+
+b = 0
+
+while b < 1000:
+    a = int(input("Escriba un número"))
+    b = b + a
+    print('La suma de los números escritos es ' + str(b))
+
+
+print('La suma de los números escritos es 1000 o más: ' + str(b))
