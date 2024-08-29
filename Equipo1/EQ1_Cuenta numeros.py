73801ee80b60b7dc3f249c26ff76554c4fef968e
andreamestizo <A01735773@tec.mx>
2024-08-29 17:38:33 -0600

    Add files via upload

---
+++ Equipo1/EQ1_Cuenta numeros.py
@@ -0,0 +1,5 @@
+n = int(input("Ingresa un número entero positivo: "))
+x = 0
+while x<n:
+    x = x+1
+    print(x)
---
+++ Equipo1/EQ1_Suma numeros.py
@@ -0,0 +1,7 @@
+n = None
+suma = 0
+print("Ingresa números que quieras sumar, cuando quieras detener ingresa un 0")
+while n!=0:
+    n = int(input("Ingresa un número: "))
+    suma = suma + n
+print(suma)
