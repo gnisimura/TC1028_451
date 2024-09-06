c570b97a643631c54efeaa8b5d558ebd24518cf6
AnnetteChoque <A00842260@tec.mx>
2024-09-05 18:03:12 -0600

    Add files via upload

---
+++ Equipo3/EQ 3_Examen Practico_Triangulo.py
@@ -0,0 +1,16 @@
+def dibuja_linea(caracter,cantidad):
+    if cantidad>0:
+        print(caracter*cantidad)
+    elif cantidad<0:
+        print('El valor no puedes ser negativo')
+
+n=int(input('Ingresa la cantidad de lineas que desees: '))
+caracter=input('Ingresa un caracter: ')
+
+def main():
+    
+    for cantidad in range(1, n + 1):
+        dibuja_linea(caracter,cantidad)
+
+print('')
+main()
