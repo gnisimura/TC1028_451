f47ec223224a8dc7d534875d99c72d54c15b03f9
IsaacMontanoManriquez <a00841681@tec.mx>
2024-09-26 18:08:51 -0600

    Add files via upload

--- Equipo4/EQ4_ParesEnCadaRenglon.py
+++ Equipo4/EQ4_ParesEnCadaRenglon.py
@@ -1,20 +1,22 @@
-col = int(input('Inserte el numero de columnas'))
-fil = int(input('Inserte el numero de filas'))
+col=int(input("Columnas?"))
+fil=int(input("Filas?"))
 matriz=[]
-for i in range(fil):
-    matriz.append([])
-
-for j in range(fil):
-    for k in range(col):
-        print(f'Inserte el valor en la fila {j+1} y columna {k+1}')
-        a = int(input())
-        matriz[j].append(a)
-cont2 = 0 
-for h in matriz:
-    cont = 0
-    cont2 +=1
-    for n in range(col):
-        if h[n]%2 == 0:
-            cont +=1
-    print(f'El numero de pares en el renglon {cont2} es {cont}')
+for i in range (fil):
+    fila=[]
+    for j in range (col):
+        dato=int(input("Dato en la fila "+ str(i+1) +" y columna "+str(j+1)))
+        fila.append(dato)
+    matriz.append(fila)
     
+print(matriz) 
+listapares=[]
+for i in range(fil):
+    pares=0
+    for j in range(col):
+        if int(matriz[i][j])%2==0:
+            pares+=1
+    listapares.append(pares)
+    
+print(listapares)
+    
+        
