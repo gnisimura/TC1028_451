172f56809d83c0a3856428fede5728ca037adfec
Miguel Angel Hernández Ochoa <A00830949@tec.mx>
2024-09-30 17:40:41 -0600

    Update EQ 1_Fragmentos de Strings.py

--- Equipo1/EQ 1_Fragmentos de Strings.py
+++ Equipo1/EQ 1_Fragmentos de Strings.py
@@ -1,6 +1,6 @@
 string = input("Inserte una palabra: ")
 
 print("Longitud de la palabra:",len(string))
-print("Primer caracter:",string[1])
+print("Primer caracter:",string[0])
 print("Último caracter:",string[-1])
 print("Caracteres con índice impar:",string[1:-1:2])
