0775f94633de005f778212fde92d06d153c11b7e
gnisimura <german.nisimura@tec.mx>
2024-09-30 17:30:46 -0600

    Update Eq_Datos de una Esfera.py

--- Equipo2/Eq_Datos de una Esfera.py
+++ Equipo2/Eq_Datos de una Esfera.py
@@ -2,9 +2,9 @@ from math import pi
 
 radio = int(input("Ingrese el radio de la esfera"))
 
-area = 4 * pi * radio^2
+area = 4 * pi * radio**2
 
-volumen = (4 * pi * radio^3) / 3
+volumen = (4 * pi * radio**3) / 3
 
 print("Area  = " + str(area))
 print("Volumen = " + str(volumen))
