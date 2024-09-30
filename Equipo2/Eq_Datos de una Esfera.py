c5806b5a9293987853c2a9721ef28f77d4ddac6d
A00841165 <a00841165@tec.mx>
2024-09-30 11:27:37 -0600

    Create Eq_Datos de una Esfera.py

---
+++ Equipo2/Eq_Datos de una Esfera.py
@@ -0,0 +1,10 @@
+from math import pi
+
+radio = int(input("Ingrese el radio de la esfera"))
+
+area = 4 * pi * radio^2
+
+volumen = (4 * pi * radio^3) / 3
+
+print("Area  = " + str(area))
+print("Volumen = " + str(volumen))
