576fb6a89e3165ac7f3618fe37b3228036372051
A00841165 <a00841165@tec.mx>
2024-09-30 17:41:57 -0600

    Create Eq2_Fragmentos de String.py

---
+++ Equipo2/Eq2_Fragmentos de String.py
@@ -0,0 +1,15 @@
+texto = str(input("Ingrese texto: "))
+
+#longitud del string
+print(len(texto))
+
+#primer caracter
+print(texto[0])
+
+#ultimo caracter
+print(texto[-1])
+
+#caracteres con indice impar
+for x in range(0, len(texto)):
+    if x % 2 == 0:
+        print(texto[x], end="")
